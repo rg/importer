9a43cda4ce9543186718599787b610f2c8d18fa0
Rohit <rohitkg@gmail.com>
2012-06-16 21:12:17 -0700

    proper rails upgrade, and mods for asset pipeline

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Importer::Application.config.secret_token = 'e12816ebba93d4a22c3397715d4ee699b1451b4fde24bbcfb4176c2333cb01b9a60644a5ca8642403b6c7e1bb811d2032104009935fe28e21af4d56e58990c0b'
+Importer::Application.config.secret_token = '15d96944c680f7445d515b7f0a88524adc8bbc6fe09a6ff067cc094b6ad3b040aceb84be9d535ed6c61fa3638de0936ba1750fcc473cd3d8f39a2fce2828589c'
